f5c102d3b484f021fd5c6383c5b3a39ceb679556
Mark <markind@hotmail.com>
2019-08-16 15:53:22 -0700

    Fixes remaining PEP8 compliance issues in EnigmaTest.py

--- EnigmaTest.py
+++ EnigmaTest.py
@@ -2,6 +2,7 @@ import unittest
 from configparser import ConfigParser
 from Enigma import Enigma
 
+
 class TestEnigma(unittest.TestCase):
     def createConfig(self):
         config = ConfigParser()
@@ -22,30 +23,34 @@ class TestEnigma(unittest.TestCase):
         config['rotors']['rotor2Ring'] = '0'
         config['rotors']['rotor3Ring'] = '0'
 
-        config['plugboard'] = {'a':'M','e':'T'}
+        config['plugboard'] = {'a': 'M', 'e': 'T'}
 
         return config
 
     def testRotorIds(self):
         e = Enigma(self.createRotorConfig())
-        assert e.rotor1.id == 'I', "Failed to assign rotor correctly"
+        assert e.rotor1.id == 'I', \
+            "Failed to assign rotor correctly"
 
     def testRotorPositions(self):
         e = Enigma(self.createRotorConfig())
-        assert e.rotor1.position == 0, "Failed to assign rotor position correctly"
+        assert e.rotor1.position == 0, \
+            "Failed to assign rotor position correctly"
 
     def testAdvanceRightRotor(self):
         e = Enigma(self.createRotorConfig())
         pos1 = e.rotor1.position
         e.encode('a')
-        assert e.rotor1.position == pos1 + 1, "Failed to advance right rotor on encode"
+        assert e.rotor1.position == pos1 + 1, \
+            "Failed to advance right rotor on encode"
 
     def testAdvanceMiddleRotor(self):
         e = Enigma(self.createRotorConfig())
         e.rotor1.position = 25
         pos2 = e.rotor2.position
         e.encode('a')
-        assert e.rotor2.position == pos2 + 1, "Failed to advance middle rotor on encode"
+        assert e.rotor2.position == pos2 + 1, \
+            "Failed to advance middle rotor on encode"
 
     def testAdvanceLeftRotor(self):
         e = Enigma(self.createRotorConfig())
@@ -53,7 +58,8 @@ class TestEnigma(unittest.TestCase):
         e.rotor2.position = 25
         pos3 = e.rotor3.position
         e.encode('a')
-        assert e.rotor3.position == pos3 + 1, "Failed to advance left rotor on encode"
+        assert e.rotor3.position == pos3 + 1, \
+            "Failed to advance left rotor on encode"
 
     def testEncodeSimple(self):
         e = Enigma(self.createRotorConfig())
@@ -75,14 +81,14 @@ class TestEnigma(unittest.TestCase):
         assert e.plugboard.get_wiring('T') == 'E'
         assert e.plugboard.get_wiring('X') == 'X'
 
-    # I was able to verify these encodings against the enigmaco.de emulator, among
-    # others, with identical settings.
+    # I was able to verify these encodings against the enigmaco.de emulator,
+    # among others, with identical settings.
     #
-    # Note that "HELLO" will not necessarily encode to "AMNCZ" with these same
-    # settings in other emulators, depending on the wiring of the rotors and other
-    # variations. While this matches a few different emulators out there and fits
-    # to my very imperfect understanding of how the Enigma worked, I make no claim
-    # that the results are historically accurate.
+    # Note that "HELLO" will not necessarily encode to "AMNCZ" with these
+    # same settings in other emulators, depending on the wiring of the rotors
+    # and other variations. While this matches a few different emulators out
+    # there and fits to my very imperfect understanding of how the Enigma
+    # worked, I make no claim that the results are historically accurate.
     def testString(self):
         e = Enigma(self.createRotorConfig())
         msg = e.encode('HELLO')
@@ -92,6 +98,7 @@ class TestEnigma(unittest.TestCase):
         e = Enigma(self.createRotorConfig())
         decode = e.encode(msg)
         assert decode == 'HELLO', "Decoded large message improperly"
+
 
 if __name__ == '__main__':
     unittest.main()
